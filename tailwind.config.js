1a26fd6275890ed583f362d399632d8d5c684ce2
tanjir-mahabub <md.tanjir.mahabub@gmail.com>
2024-11-28 16:38:24 +0600

    Updated all sections

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./layout/**/*.liquid', './sections/**/*.liquid', './snippets/**/*.liquid', './templates/**/*.liquid'], 
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
